e2fbf25e2c3c23a7ef6df4a315ef5596cfb60332
meluchoMZ <miguelgodon@outlook.es>
2021-02-05 20:28:40 +0100

    Refactor, solved bug when multiple data in one line on config files

--- fetchme.c
+++ fetchme.c
@@ -17,11 +17,12 @@
 
 
 void fetch(void);
-char * get_os(void);
-char * get_kernel(void);
-char * get_uptime(void);
-char * get_CPU_info(void);
-char * get_mem_info(void);
+void get_os(void);
+void get_kernel(void);
+void get_uptime(void);
+void get_CPU_info(void);
+void get_mem_info(void);
+
 int main(void)
 {
 	fetch();
@@ -38,18 +39,15 @@ void fetch(void)
 	char * col4 = "\t     \t         ";
 	char * col5 = "\t  Me \tMy computer\n";
 
-	printf("%s\t OS: %s\n", col0, get_os());
-	printf("%s\t Kernel: %s\n", col1, get_kernel());
-	printf("%s\t Up: %s min\n", col2, get_uptime());
-	printf("%s\t CPU: %s\n", col3, get_CPU_info());
-	printf("%s\t Memory: ", col4);
-	printf("%s\n", get_mem_info());
+	printf("%s\t OS: ", col0); get_os();
+	printf("%s\t Kernel: ", col1); get_kernel();
+	printf("%s\t Up: ", col2); get_uptime();
+	printf("%s\t CPU: ", col3); get_CPU_info();
+	printf("%s\t Memory: ", col4); get_mem_info();
 	printf("%s\n", col5);
-
-	return;
 }
 
-char * get_os(void)
+void get_os(void)
 {
 	// A información do SO está separada en varios ficheiros
 	// Neste caso, o "nome bonito" está no ficheiro /etc/os-release
@@ -59,46 +57,43 @@ char * get_os(void)
 	char buffer[512];
 	char key[64];
 	char value[512];
-	char * ret;
 	if ((f = fopen("/etc/os-release","r")) == NULL) {
-		return "unaccesible info! :-(";
+		printf("unaccesible info! :-(\n");
 	}
 
 	while (fgets(buffer, 512, f) != NULL) {
 		sscanf(buffer, "%[^=]=\"%[^\"]", key, value);
 		if (strcmp(key, "PRETTY_NAME") == 0) {
-			ret = value;
 			fclose(f);
-			return ret;
+			printf("%s\n", value);
+			return;
 		}
 	}
 	fclose(f);
 
-	return "unaccesible info! :-(";
+	printf("unaccesible info! :-(\n");
 }
 
-char * get_kernel(void)
+void get_kernel(void)
 {
 	// A información da versión do kernel está no ficheiro /proc/version
 	// Kernel version information is in /proc/version file
 	FILE * f;
 	char buffer[2048];
 	char value[1024];
-	char * ret;
 	if ((f = fopen("/proc/version","r")) == NULL) {
-		return "unaccesible info! :-(";
+		printf("unaccesible info! :-(\n");
 	}
 
 	if (fgets(buffer, 2048, f) == NULL) {
-		return "unaccesible info! :-(";
+		printf("unaccesible info! :-(\n");
 	}
 	sscanf(buffer, "%[^(]", value);
-	ret = value;
 
-	return ret;
+	printf("%s\n", value);
 }
 
-char * get_uptime(void)
+void get_uptime(void)
 {
 	// A información de tempo acendido está gardada no ficheiro 
 	// /proc/uptime. Garda o tempo en segundos
@@ -108,14 +103,13 @@ char * get_uptime(void)
 	char  buffer[64];
 	double uptime = 0;
 	char  upt[64];
-	char * rupt;
 
 	if ((fup = fopen("/proc/uptime","r")) == NULL) {
-		return "unaccesible info! :-(";
+		printf("unaccesible info! :-(\n");
 	}
 
 	if (fgets(buffer, 64, fup) == NULL) {
-		return "unaccesible info! :-(";
+		printf("unaccesible info! :-(\n");
 	}
 
 	sscanf(buffer, "%lf", &uptime);
@@ -126,11 +120,10 @@ char * get_uptime(void)
 	// converting uptime to minutes
 	uptime = round(uptime / 60);
 	snprintf(upt, 64, "%d", (int) uptime);
-	rupt = upt;
-	return rupt;
+	printf("%s min\n", upt);
 }
 
-char * get_CPU_info(void)
+void get_CPU_info(void)
 {
 	// A información do procesador está en /proc/cpuinfo
 	// CPU information is stored in /proc/cpuinfo
@@ -138,25 +131,24 @@ char * get_CPU_info(void)
 	char buffer[1024];
 	char key[128];
 	char value[1024];
-	char * ret;
 
 	if ((f = fopen("/proc/cpuinfo","r")) == NULL) {
-		return "unaccesible info! :-(";
+		printf("unaccesible info! :-(\n");
 	}
 
 	while (fgets(buffer, 1024, f) != NULL) {
 		sscanf(buffer, "%[^:]:%[^\n]", key, value);
 		if (strncmp(key, "model name", 10) == 0) {
 			fclose(f);
-			ret = value;
-			return ret;
+			printf("%s\n", value);
+			return;
 		}
 	}
 	fclose(f);
-	return "unaccesible info! :-(";
+	printf("unaccesible info! :-(\n");
 }
 
-char * get_mem_info(void)
+void get_mem_info(void)
 {
 	// A información da memoria está no ficheiro /proc/meminfo
 	// Memory information is in /proc/meminfo
@@ -165,10 +157,9 @@ char * get_mem_info(void)
 	char key[64];
 	int total=0, available;
 	char mem_ratio[64];
-	char * ret;
 
 	if ((f = fopen("/proc/meminfo","r")) == NULL) {
-		return "unaccesible info! :-(";
+		printf("unaccesible info! :-(\n");
 	}
 
 	while (fgets(buffer, 128, f) != NULL) {
@@ -179,10 +170,10 @@ char * get_mem_info(void)
 	}
 	fclose(f);
 	if (total == 0) {
-		return "missing information! :-(";
+		printf("unaccesible info! :-(\n");
 	}
 	if ((f = fopen("/proc/meminfo","r")) == NULL) {
-		return "unaccesible info! :-(";
+		printf("unaccesible info! :-(\n");
 	}
 
 	while (fgets(buffer, 128, f) != NULL) {
@@ -198,7 +189,5 @@ char * get_mem_info(void)
 	available = available / 1000;
 
 	snprintf(mem_ratio, 64,  "%d MB / %d MB", total - available, total);
-	ret = mem_ratio;
-
-	return ret;
+	printf("%s\n", mem_ratio);
 }
